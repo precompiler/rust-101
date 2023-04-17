498d4a69c6f1638b0a19d63237155034c26a3eab
precompiler <lishengyin.msn@hotmail.com>
2023-04-17 21:45:56 +0100

    data types

--- src/commonConcepts.rs
+++ src/commonConcepts.rs
@@ -18,5 +18,24 @@ fn main() {
     let mut x = 1;
     x += i;
     println!("x = {x}");
-    println!("{TWENTY_ONE_LOCAL} - {ONE_GLOBAL_SCOPE} = ?")
+    println!("{TWENTY_ONE_LOCAL} - {ONE_GLOBAL_SCOPE} = ?");
+    // data types
+    let a: i32 = 0;
+    let b: bool = true;
+    let c: char = 'x';
+    let d/*:(i32, char, f32)*/ = (1, 'y', 1.1f32);
+    let (x, y, z) = d;
+    let x1 = d.0;
+    let x2 = d.1;
+    let x3 = d.2;
+    println!("{a}, {b}, {c}, {:?}", d); // d is of type tuple, which is a compound type, and cannot be formatted using {}
+    println!("{x}{y}{z}");
+    println!("{x1}{x2}{x3}");
+
+    let fib = [1, 1, 2, 3, 5]; // fixed length array
+    println!("{}", fib[0]);
+    let fib: [i32; 5] = [1, 1, 2, 3, 5]; // explicit typed
+    println!("{:?}", fib);
+    let dup1s = [1; 10]; // 10 1s in the array
+    println!("{:?}", dup1s)
 }
