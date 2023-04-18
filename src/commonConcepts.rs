343e4fd41ace57ffde95c92f0d429d8a67860f34
precompiler <lishengyin.msn@hotmail.com>
2023-04-18 14:01:08 +0100

    flow control

--- src/commonConcepts.rs
+++ src/commonConcepts.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 // constant declared in global scope
 // type must be specified
 const ONE_GLOBAL_SCOPE: u16 = 1;
@@ -37,5 +38,111 @@ fn main() {
     let fib: [i32; 5] = [1, 1, 2, 3, 5]; // explicit typed
     println!("{:?}", fib);
     let dup1s = [1; 10]; // 10 1s in the array
-    println!("{:?}", dup1s)
+    println!("{:?}", dup1s);
+    let sum = add(1, 2);
+    println!("{sum}");
+    let v = substract(5, 3);
+    println!("{v}");
+    let fib_ret = calc_fib_recursive(20);
+    println!("{fib_ret}");
+    dup_print_10_times(100);
+
+    let idx = find(10, [1, 2, 3, 4, 5]);
+    println!("{idx}");
+
+    let idx = find(3, [1, 2, 3, 4, 5]);
+    println!("{idx}");
+
+    loop_label();
+    while_loop();
+    for_loop_array();
+    for_range(8, false);
+    for_range(8, true);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b //no semicolon indicates this is a expression not a statement, expression will have a return value
+}
+
+fn substract(a: i32, b: i32) -> i32 {
+    return a - b;
+}
+
+fn calc_fib_recursive(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return calc_fib_recursive(n - 1) + calc_fib_recursive(n - 2);
+    }
+}
+
+fn dup_print_10_times(n: i32) {
+    let mut i = 0;
+    loop {
+        println!("{n}");
+        i += 1;
+        if i == 10 {
+            break;
+        }
+    }
+}
+
+fn find(n: i32, t: [i32; 5]) -> i32 {
+    let mut idx = -1;
+    if t.len() == 0 {
+        return idx;
+    }
+    let mut i = 0;
+    idx = loop {
+        if n == t[i] {
+            break i.try_into().unwrap(); // break can return value for the loop
+        }
+        i += 1;
+        if i == t.len() {
+            break -1;
+        }
+    };
+    return idx;
+}
+
+fn loop_label() {
+    let mut i = 0;
+    'outer_loop: loop {
+        loop {
+            println!("in loop");
+            i += 1;
+            if i > 5 {
+                break 'outer_loop;
+            }
+        }
+    }
+}
+
+fn while_loop() {
+    let mut i = 0;
+    while i < 3 {
+        println!("{i}");
+        i += 1;
+    }
+}
+
+fn for_loop_array() {
+    let x = [1, 2, 3, 4, 5];
+    for n in x {
+        println!("{n}");
+    }
+}
+
+fn for_range(n: i32, reverse: bool) {
+    if reverse {
+        for i in (1..n).rev() {
+            println!("({i}");
+        }
+    } else {
+        for i in 1..n {
+            println!("({i}");
+        }
+    }
 }
