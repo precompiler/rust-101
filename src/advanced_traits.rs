0bd8e0da2d9ba79f36dfe3f7aa771a9f0b0457f6
precompiler <lishengyin.msn@hotmail.com>
2023-06-03 22:26:47 +0100

    advanced traits

--- src/advanced_traits.rs
+++ src/advanced_traits.rs
@@ -1,4 +1,4 @@
-
+use std::ops::Add;
 
 fn main() {
     let mut c = Counter{c: 10};
@@ -9,6 +9,22 @@ fn main() {
 
     let mut c = Counter2{_c: 1};
     println!("{}", <Counter2 as GenericIterator<String>>::next(&mut c).unwrap());
+
+    let cn1 = ComplexNumber{r: 1.0, i: 2.0};
+    let cn2 = ComplexNumber{r: 2.0, i: 3.0};
+    let cn_sum = cn1 + cn2;
+    println!("{} + {}i", cn_sum.r, cn_sum.i);
+    let cn3 = ComplexNumber{r: 3.0, i: 1.0};
+    let cn_sum2 = cn3 + 3.0;
+    println!("{} + {}i", cn_sum2.r, cn_sum2.i);
+
+    let player = SpotifyPlayer;
+    player.play();
+    DVDPlayer::play(&player);
+    MP3Player::play(&player);
+
+    JVM::shut_down();
+    <JVM as PowerManager>::shut_down();
 }
 
 trait GenericIterator<T> {
@@ -54,6 +70,8 @@ impl Iterator for i32 {
     }
 }
 
+
+
 impl GenericIterator<i32> for Counter2 {
     fn next(&mut self) -> Option<i32> {
         return Some(100);
@@ -63,5 +81,75 @@ impl GenericIterator<i32> for Counter2 {
 impl GenericIterator<String> for Counter2 {
     fn next(&mut self) -> Option<String> {
         return Some(String::from("hello"));
+    }
+}
+
+struct ComplexNumber {
+    r: f32,
+    i: f32
+}
+
+impl Add for ComplexNumber {
+    type Output = ComplexNumber;
+    fn add(self, rhs: Self) -> Self::Output {
+        return ComplexNumber {
+            r: self.r + rhs.r,
+            i: self.i + rhs.i
+        }
+    }
+}
+
+impl Add<f32> for ComplexNumber {
+    type Output = ComplexNumber;
+    fn add(self, rhs: f32) -> Self::Output {
+        return ComplexNumber {
+            r: self.r + rhs,
+            i: self.i
+        }
+    }
+}
+
+trait DVDPlayer {
+    fn play(&self);
+}
+
+trait MP3Player {
+    fn play(&self);
+}
+
+struct SpotifyPlayer;
+
+impl SpotifyPlayer {
+    fn play(&self) {
+        println!("Spotify play...")
+    }
+}
+
+impl DVDPlayer for SpotifyPlayer {
+    fn play(&self) {
+        println!("DVD play...");
+    }
+}
+
+impl MP3Player for SpotifyPlayer {
+    fn play(&self) {
+        println!("MP3 play...");
+    }
+}
+
+trait PowerManager {
+    fn shut_down();
+}
+
+struct JVM;
+impl JVM {
+    fn shut_down() {
+        println!("shutting down JVM");
+    }
+}
+
+impl PowerManager for JVM {
+    fn shut_down() {
+        println!("shutting down JVM and turning off PC");
     }
 }
